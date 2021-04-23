1206d9d2c1023692e562937a2c7c524022a0352a
alinearaujo-si <alinearaujo.si.ufc@gmail.com>
2021-04-22 23:43:49 -0300

    Fundamentos

---
+++ fundamentos/strings.js
@@ -0,0 +1,6 @@
+const escola = "UFC"
+
+console.log(escola.charAt(0))
+console.log(escola.charAt(2)) //imprimindo os caracteres
+console.log(escola.charCodeAt(2)) //imprime o valor ASCII relacionado
+console.log(escola.indexOf('U')) //identifica o indice relacionado a esse caracytere
